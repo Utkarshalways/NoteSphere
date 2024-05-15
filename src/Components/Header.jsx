a5f1bc1a20b872156f9b9f34d301235a1c8db01f
Utkarshalways <utkarshshrma08@gmail.com>
2024-05-15 22:53:58 +0530

    header updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,159 +1,80 @@
 import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import closesvg from "../../Public/close.svg";
+import hamburger from "../../Public/hamburger-menu.svg";
 import { useFirebase } from "../context/Firebasecontextprovider";
+// import "./Style/Header.css"
 
 const Header = () => {
-  const [navOpen, setnavOpen] = useState(false);
+  
   const firebase = useFirebase();
-  // console.log(firebase);
+  const [navbar, setNavbar] = useState(false);
+  
   return (
-    <header className="bg-purple-800 p-4 flex gap-2 items-center justify-between text-white sticky">
-      <div className="left flex gap-2 items-center ">
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/3/33/Vanamo_Logo.png"
-          alt=""
-          className="h-8"
-        />
-        <p className=" font-bold text-l sm:text-sm">NOTESPHERE</p>
-      </div>
-      <section>
-        <div
-          className="HAMBURGER-ICON space-y-2 hidden  sm:inline "
-          onClick={() => setnavOpen((prev) => !prev)} // toggle isNavOpen state on click
-        >
-          {/* {console.log(navOpen)} */}
-          <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
-          <span className="block h-0.5 w-8 animate-bounce bg-white"></span>
-          <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
-        </div>
-
-        <div className={navOpen ? "showMenuNav bg-purple-400 " : "hideMenuNav"}>
-          {" "}
-          <div
-            className="CROSS-ICON absolute top-0 right-0 px-8 py-8"
-            onClick={() => setnavOpen(false)} // change isNavOpen state to false to close the menu
-          >
-            <svg
-              className="h-8 w-8 text-black"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <line x1="18" y1="6" x2="6" y2="18" />
-              <line x1="6" y1="6" x2="18" y2="18" />
-            </svg>
+    <div>
+      <nav className="w-full bg-black fixed top-0 left-0 right-0 z-10">
+        <div className="justify-between px-4 mx-auto  md:items-center md:flex md:px-8 ">
+          <div>
+            <div className="flex items-center justify-between py-3 md:py-5 md:block">
+              {/* LOGO */}
+              <Link href="/">
+                <h2 className="text-2xl text-cyan-600 font-bold ">NOTESPHERE</h2>
+              </Link>
+              {/* HAMBURGER BUTTON FOR MOBILE */}
+              <div className="md:hidden">
+                <button
+                  className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                  onClick={() => setNavbar(!navbar)}
+                >
+                  {navbar ? (
+                    <image src={closesvg} width={30} height={30} alt="logo" />
+                  ) : (
+                    <image
+                      src={hamburger}
+                      width={30}
+                      height={30}
+                      alt="logo"
+                      className="focus:border-none active:border-none"
+                    />
+                  )}
+                </button>
+              </div>
+            </div>
           </div>
-          <ul className="MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px] bg-purple-100 text-black p-4">
-            <li className="border-b border-gray-400 my-8 uppercase">
-              <Link to={"/"}>Home</Link>
-            </li>
-            <li
-              className={
-                firebase.isLoggedin
-                  ? "border-b border-gray-400 my-8 uppercase"
-                  : "hidden"
-              }
+          <div>
+            <div
+              className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+                navbar ? 'p-12 md:p-0 block' : 'hidden'
+              }`}
             >
-              <Link to={"/noteform"}>Form</Link>
-            </li>
-            <li
-              className={
-                firebase.isLoggedin
-                  ? "hidden"
-                  : "border-b border-gray-400 my-8 uppercase"
-              }
-            >
-              <Link to={"/signup"}>Sign-Up</Link>
-            </li>
-            <li
-              className={
-                firebase.isLoggedin
-                  ? "hidden"
-                  : "border-b border-gray-400 my-8 uppercase"
-              }
-            >
-              <Link to={"/signin"}>Sign-In</Link>
-            </li>
-          </ul>
+              <ul className="h-screen md:h-auto items-center justify-center md:flex ">
+                <li className="pb-6 text-xl text-white py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-purple-900  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                  <Link href="#about" onClick={() => setNavbar(!navbar)}>
+                    About
+                  </Link>
+                </li>
+                <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                  <Link href="#blog" onClick={() => setNavbar(!navbar)}>
+                    Blogs
+                  </Link>
+                </li>
+                <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                  <Link href="#contact" onClick={() => setNavbar(!navbar)}>
+                    Contact
+                  </Link>
+                </li>
+                <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                  <Link href="#projects" onClick={() => setNavbar(!navbar)}>
+                    Projects
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-      </section>
-      <div className={navOpen ? "hidden" : " sm:hidden"}>
-        <ul
-          className="flex gap-2 items-center 
-       "
-        >
-          <li className=" m-2 cursor-pointer hover:underline">
-            <Link to={"/"}>Home</Link>
-          </li>
-          
-          <li
-            className={
-              firebase.isLoggedin ? " m-2 cursor-pointer hover:underline" : "hidden"
-            }
-          >
-            <Link to={"/noteform"}>Form</Link>
-          </li>
-          <li
-            className={
-              firebase.isLoggedin
-                ? "hidden"
-                : " m-2 cursor-pointer hover:underline"
-            }
-          >
-            <Link to={"/signin"}>Sign-In</Link>
-          </li>
-          <li
-            className={
-              firebase.isLoggedin
-                ? "hidden"
-                : "  bg-blue-500 p-1 rounded-lg hover:bg-blue-600"
-            }
-          >
-            <Link to={"/signup"}>Sign-Up</Link>
-          </li>
-          <li
-            className={
-              firebase.isLoggedin
-                ? " m-2 cursor-pointer hover:underline"
-                : "hidden"
-            }
-          >
-            <button
-              onClick={firebase.logout}
-              className="  bg-red-500 p-1 rounded-lg hover:bg-red-600"
-            >
-              Log-Out
-            </button>
-          </li>
-        </ul>
-      </div>
-      <style>{`
-      .hideMenuNav {
-        display: none;
-      }
-      .showMenuNav {
-        display: block;
-        position: absolute;
-        width: 100%;
-        height: 100vh;
-        top: 0;
-        left: 0;
-        background: white;
-        z-index: 10;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-evenly;
-        align-items: center;
-        {/* opacity:0.8; */}
-        
-        
-      }
-    `}</style>
-    </header>
+      </nav>
+    </div>
   );
 };
 
